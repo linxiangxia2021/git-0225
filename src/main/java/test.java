4238d6af5c1e28765b7fac78c1c25fbe50042fd2
Shanghai210225 <1181979221@qq.com>
2021-06-28 16:29:21 +0800

    second commit

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author xlx
+ * @create 2021-06-28 16:20
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("aaa");
+    }
+}
